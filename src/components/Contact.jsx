157907be165e80022016b030f12a78300218654e
vijaykcse <vijaykcode@gmail.com>
2026-08-20 17:31:43 +0530

    contact page github URl Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -167,7 +167,7 @@ const Contact = () => {
               <FaLinkedin />
             </a>
             <a 
-              href="https://github.com/vijaykcse" 
+              href="https://github.com/vijaykworks" 
               target="_blank" 
               rel="noopener noreferrer"
               aria-label="GitHub Profile"
